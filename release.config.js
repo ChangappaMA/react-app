1839d61319b20841759753f42c9f751b510a1647
Changappa_m_a <Changappa_m_a@dell.com>
2021-10-19 14:31:08 +0530

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/ChangappaMA/react-app",
+    plugins: ["@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/git"]
+};
